1f54b85914bd212cf5deaa33fe6b0ce44bbca1c8
Ivo Ivanov <vn.n0t0@gmail.com>
2017-07-26 12:33:49 -0700

    join() method added

--- string_methods.py
+++ string_methods.py
@@ -1,16 +1,63 @@
 s = 'This is a string'
 s2 = 'This is also a string'
-s3 = 'ThIs sTrinG iS madE oF uPPer anD LoWER caSE chaRaCTeRS'
+s3 = 'ThIs stRIng S madE oF UppEr aND LOwer caSE chaRactErS'
 
-print s[:2]
-print s2[::2]
+#### indexing, cutting & slicing a string
+print '===='*40
+
+print s[:2] # cutting
+print s2[::2] # cutting 2 characters
+print s2[5:12] # slicing
+print len(s3)
+
+#### split() | join()
+print '===='*40
 
 print s.split()
 print s2.splitlines()
 
-for l in s3:
-    if l.isupper() == True:
+
+s4 = 'Abracadabra'
+print s4.split('a')
+print '--'.join(s4)
+
+s5 = "Creating a LIST from a string"
+print s5.split()
+s6 = s5.split()
+print s6
+print '_____'.join(s6)
+
+l = []
+for word in s5.split():
+    words = word.split()
+    l.append(words)
+print l
+
+print '===='*40
+
+# for w in s5:
+#     print filter(lambda w: w.split(), s5.split())
+
+
+print '===='*40
+
+for w in s5.split():
+    l = []
+    l.append(w)
+print l
+
+
+#### isupper() | islower()
+print '===='*40
+
+l = []
+for c in s3:
+    if c.isupper() == True:
+        l.append(c)
         print l
+# print '-'.join(l)
+
+print '===='*40
 
 for c in s3:
     if c.isupper() == True:
@@ -19,7 +66,6 @@ for c in s3:
         print c + ' smalls'
     else:
         print '|_|'
-#print s3
 
 
 def only_upper(s3):
@@ -31,6 +77,7 @@ def only_upper(s3):
 
 print only_upper(s3)
 print only_upper('PrintinG onlY UppeR CasE Chars')
+
 
 def only_capital(s3):
     return filter(lambda x: x.isupper(), s3)

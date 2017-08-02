43d2e75a9402400313f0c0166bc0a04b72786313
Ivo Ivanov <vn.n0t0@gmail.com>
2017-08-02 13:07:04 -0700

    RE updated

--- re-module.py
+++ re-module.py
@@ -6,23 +6,40 @@ import re
 # match()
 
 s = '01-test'
-matchObj = re.match('[0-9].', s, flags=0)
-if matchObj:
-    print 'Match: ', matchObj.group()
+match = re.match('[0-9].', s, flags=0)
+if match:
+    print 'Match: ', match.group()
 
 s = 'This is a test'
-matchObj = re.match('^this is (.) (.*)' , s, re.I|re.M)
-if matchObj:
-    print 'Matched: ', matchObj.group()
-    print 'Matched: ', matchObj.group(1)
-    print 'Matched: ', matchObj.group(2)
+match= re.match('^this is (.) (.*)' , s, re.I|re.M)
+if match:
+    print 'Matched:\t', match.group()
+    print 'Matched:\t', match.group(1)
+    print 'Matched:\t', match.group(2)
 
 # search()
 
 s = 'This is only a test'
-search = re.search('only', s, re.I|re.M)
+search = re.search('^this is (.*) (.) (.*)', s, re.I|re.M)
 if search:
-    print "Found: ", search.group()
-    # print "Found: ", search.group(1)
-    # print "Found: ", search.group(2)
-    # print "Found: ", search.group(3)
+    print "Found:\t", search.group()
+    print "Found:", search.group(1)
+    print "Found: ", search.group(2)
+    print "Found: ", search.group(3)
+
+# match() Vs. search()
+# match() - beginning of the string
+# search() - anywhere in the string
+
+s = 'Can you spot the difference?'
+match = re.match('spot', s, re.I|re.M)
+if match:
+    print 'Matched: ', match.group()
+else:
+    print 'Sorry. No match!'
+
+search = re.search('spot', s, re.I|re.M)
+if search:
+    print 'Found: ', search.group()
+else:
+    print 'Nothing found!'

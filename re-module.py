7cc5bfdb7819aec060bd349a66c6999175340cc2
Ivo Ivanov <vn.n0t0@gmail.com>
2017-08-02 12:54:00 -0700

    RE - updated

--- re-module.py
+++ re-module.py
@@ -1,17 +1,28 @@
 import re
 
-p = re.compile('ab*')
-print p
+# p = re.compile('ab*')
+# print p
+
+# match()
 
 s = '01-test'
 matchObj = re.match('[0-9].', s, flags=0)
 if matchObj:
     print 'Match: ', matchObj.group()
 
-
 s = 'This is a test'
-matchObj = re.search('^this is a (.*)' , s, re.I|re.M)
+matchObj = re.match('^this is (.) (.*)' , s, re.I|re.M)
 if matchObj:
-    print 'Match: ', matchObj.group()
-    print 'Match: ', matchObj.group(1)
-    # print 'Match: ', matchObj.group(2)
+    print 'Matched: ', matchObj.group()
+    print 'Matched: ', matchObj.group(1)
+    print 'Matched: ', matchObj.group(2)
+
+# search()
+
+s = 'This is only a test'
+search = re.search('only', s, re.I|re.M)
+if search:
+    print "Found: ", search.group()
+    # print "Found: ", search.group(1)
+    # print "Found: ", search.group(2)
+    # print "Found: ", search.group(3)

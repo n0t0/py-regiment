3820efa0fe5e7741728bcf05a5fa58bc1135f25d
Ivo Ivanov <vn.n0t0@gmail.com>
2017-08-07 17:36:21 -0700

    Ex_04 - solved

--- exercises/ex_04.py
+++ exercises/ex_04.py
@@ -11,7 +11,6 @@
 
 # 1
 
-
 def do_twice(f):
     f()
     f()
@@ -22,16 +21,48 @@ def print_spam():
 
 do_twice(print_spam)
 
-
 # 2
 
-v = 'Fight'
+v = 'variable'
 def do_twice(f, v):
     f(v)
     f(v)
 
 
 def print_spam(v):
-    print v
+    print 'spam'
 
 do_twice(print_spam, v)
+
+# 3
+
+def print_twice(krakra):
+    print krakra
+    print krakra
+
+print print_twice('double trouble')
+
+# 4
+
+def do_twice(f, v):
+    f(v)
+    f(v)
+
+
+def print_twice(krakra):
+    print krakra
+    print krakra
+
+print do_twice(print_twice,'spam')
+
+# 5
+
+def print_twice(krakra):
+    print krakra
+    print krakra
+
+def do_four(op, trop):
+    op(trop)
+    op(trop)
+
+print do_four(print_twice, 'kappa')

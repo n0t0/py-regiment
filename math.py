56d58e77d08c1b61fc9227d99a76c13467e806cf
Ivo Ivanov <vn.n0t0@gmail.com>
2017-08-09 11:46:10 -0700

    Math - added

---
+++ math.py
@@ -0,0 +1,28 @@
+# Order of operations
+
+# PEMDAS acronym
+
+# Parantheses
+
+print (3-1) * 2 + (4-2) ** 2
+
+# Exponent
+
+print 2 ** 2 + 1
+print 3 ** 1 + 3
+
+# Multiplication / Devision
+
+print 2 * 3 - 1
+print 4 * 4 - 4
+
+# Addition / Substraction
+
+print 4 - 1
+
+# String concatenation
+
+s = 'String'
+print s + s
+print s * 3
+print s ** 3 

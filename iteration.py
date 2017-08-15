1620cd9d11751903c8f8858a2a12f287adea948c
Ivo Ivanov <vn.n0t0@gmail.com>
2017-08-14 18:33:31 -0700

    iteration.py - added

---
+++ iteration.py
@@ -0,0 +1,43 @@
+# updating variables
+
+x = 0
+x = x + 1
+print x
+
+# while statemenet
+def countdown(n):
+    while n > 0:
+        print n
+        n = n-1
+    print 'Fire in the hole!'
+
+print countdown(5)
+
+
+def sequence(n):
+    while n != 1:
+        print n,
+        if n%2 == 0:
+            n = n/2
+        else:
+            n = n*3+1
+
+print sequence(10)
+
+# break
+while True:
+    line = raw_input('> ')
+    if line == 'done':
+        break
+    print line
+
+print 'Done!'
+
+# square roots
+
+# Newton's method for computing square roots: y = (x + a/x) / 2
+
+a = 4.0
+x = 3.0
+y = (x + a/x) / 2
+print y

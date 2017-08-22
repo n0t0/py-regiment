fbaa90a41416844e3de0ff630830f04b41116f47
Ivo Ivanov <vn.n0t0@gmail.com>
2017-08-22 11:29:29 -0700

    Classes - updated

--- classes.py
+++ classes.py
@@ -32,3 +32,27 @@ print print_point(blank)
 
 # instide the function p is an alias for blank;
 # if the function changes p, blank changes
+
+
+class Rectangle(object):
+    """Represents a rectangle.
+
+    attrubutes: width, height, corner
+    """
+
+box = Rectangle()
+box.width = 100.0
+box.height = 200.0
+box.corner = Point()
+box.corner.x = 0.0
+box.corner.y = 0.0
+
+
+def find_center(rect):
+    p = Point()
+    p.x = rect.corner.x + rect.width/2.0
+    p.y = rect.corner.y + rect.height/2.0
+    return p
+
+center = find_center(box)
+print_point(center)

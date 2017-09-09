ea09c792b297f4a749c6be97865f1286f40526a5
Ivo Ivanov <vn.n0t0@gmail.com>
2017-09-08 20:37:58 -0700

    files_ex03 - added

---
+++ exercises/Files_Ex03.py
@@ -0,0 +1,21 @@
+# Exercise 2
+# Write a function called sed that takes as arguments a pattern string,
+# a replacement string, and two filenames; it should read the first file
+# and write the contents into the second file (creating it if necessary).
+# If the pattern string appears anywhere in the file, it should be replaced
+# with the replacement string.
+
+# If an error occurs while opening, reading, writing or closing files,
+# your program should catch the exception, print an error message, and exit.
+
+import os
+
+cwd = os.getcwd()
+print cwd
+
+f1 = open('Files_file1.txt', 'r')
+print f1.read()
+
+# def sed(string, r_string, f1, f2):
+#     open('file1.txt')
+#     open('file2.txt', 'w')

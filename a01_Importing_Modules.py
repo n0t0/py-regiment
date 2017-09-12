7210c2ac81ba68da7453fe51b9e51e0e8ca6eec7
Ivo Ivanov <vn.n0t0@gmail.com>
2017-09-11 19:10:24 -0700

    importing modules - file rename

--- a01_Importing_Modules.py
+++ a01_Importing_Modules.py
@@ -3,14 +3,19 @@
 import math
 
 print math.sqrt(4)
+
 print help(math.pow)
-print math.pow(2,4)
+print math.pow(2, 4)
+print math.pow(2, 10)
+
 
 # Importing module as a alias
 
 import time as t
 
 print t.time()
+print t.clock()
+print t.localtime()
 
 # Importing specific function from a module allows calling its function only
 

fe798c966f3d94802b990022283271fb440f4a7a
Ivo Ivanov <vn.n0t0@gmail.com>
2017-09-12 12:52:39 -0700

    inheritance - updated

--- 15_Inheritance.py
+++ 15_Inheritance.py
@@ -9,9 +9,30 @@ class Card(object):
         self.suit = suit
         self.rank = rank
 
+
     def __str__(self):
         return '%s of %s' % (Card.rank_names[self.rank],
                              Card.suit_names[self.suit])
+
+
+    # def __cmp__(self, other):
+    #     # suits
+    #     if self.suit > other.suit: return 1
+    #     if self.suit < other.suit: return -1
+    #
+    #     # if suits are the same..check ranks
+    #     if self.rank > other.rank: return 1
+    #     if self.rank < other.rank: return -1
+    #
+    #     # if ranks are the same..it's a tie
+    #     return 0
+
+
+    def __cmp__(self, other):
+        t1 = self.suit, self.rank
+        t2 = other.suit, other.rank
+        return cmp(t1, t2)
+
 
 queen_of_diamonds = Card(1, 12)
 

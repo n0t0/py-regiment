6aa67c54ba04148d72b029192ce7edf9b9b6d61e
Ivo Ivanov <vn.n0t0@gmail.com>
2017-09-11 18:55:44 -0700

    01 - file rename

--- importing_modules.py
+++ importing_modules.py
@@ -1,16 +1,18 @@
+# Importing modules
+
 import math
 
 print math.sqrt(4)
 print help(math.pow)
 print math.pow(2,4)
 
-# importing module as a alias
+# Importing module as a alias
 
 import time as t
 
 print t.time()
 
-# importing specific function from a module allowing calling the function only
+# Importing specific function from a module allows calling its function only
 
 from calendar import firstweekday
 

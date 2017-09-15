19cbb82a65a99361ffb20c67b819db3285c78bd2
Ivo Ivanov <vn.n0t0@gmail.com>
2017-09-15 12:26:02 -0700

    inheritance - updated

--- 15_Inheritance.py
+++ 15_Inheritance.py
@@ -57,5 +57,17 @@ class Deck(object):
             res.append(str(card))
         return '\n'.join(res)
 
+
+    def pop_card(self):
+        return self.cards.pop()
+
+
+    def add_card(self, card):
+        self.cards.append(card)
+
+    # NOTE: import random module
+    # def shuffle(self):
+    #     random.shuffle(self.cards)
+
 deck = Deck()
 print deck

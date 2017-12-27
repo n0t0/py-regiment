27e04a0e08123c7c5c9da1e91f38d950b9d41efb
Ivo Ivanov <vn.n0t0@gmail.com>
2017-12-27 15:41:38 -0800

    math module added

---
+++ a06_MATH_module.py
@@ -0,0 +1,36 @@
+
+
+import math # importing a module
+
+print math # prints information about it
+print math.pi # calling a module's functions (dot notation)
+print math.sqrt(16)
+
+# ratio = signal_power / noise_power
+# decibels = 10 * math.log10(ratio)
+#
+# radians = 0.7
+# height = maht.sin(radians)
+#
+# degrees = 15
+# radians = degrees / 360.0 * 2 * math.pi
+# math.sin(radians)
+#
+# # check
+#
+# math.sqrt(2) / 2.0
+
+# NOTE: Add Math's module and functions in another chapter
+# Engine compression and compressions ratio
+
+piston = 'x'
+cylinder = 'x'
+combustionChamber = 'x'
+bottomStroke = 'x'
+topStroke = 'x'
+cc = 1000
+
+# RPM
+# AVG speed
+# Aerodynamics
+# Bike suspension

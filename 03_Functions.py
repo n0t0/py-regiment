b1eb6c4fda9838fb202bd2509dd2749993e2e193
Ivo Ivanov <vn.n0t0@gmail.com>
2017-12-27 16:54:11 -0800

    functions - clarified

--- 03_Functions.py
+++ 03_Functions.py
@@ -1,47 +1,44 @@
+print '####'*40
 # Built in Functions
+print '####'*40
+
 
 print int('32') # converts to integer
 print int(32.99) # chops the fraction part
-print int(-2.44)
+print int(-2.44) # same when negative numbers are involved
 print float(32) # converts to floating-point numbers
 print str(32) # converts to string
 print str(32.22)
 
+
+print '####'*40
 # Math Functions
+# Calling a 'dot notation' function from a module
+# module - colletion of functions
 print '####'*40
 
-# NOTE: Add Math's module and functions in another chapter
-# Engine compression
-# RPM
-# AVG speed
-# Aerodynamics
-# Bike suspension
 
-import math
-print math
+import math # importing a module
 
-# ratio = signal_power / noise_power
-# decibels = 10 * math.log10(ratio)
-#
-# radians = 0.7
-# height = maht.sin(radians)
-#
-# degrees = 15
-# radians = degrees / 360.0 * 2 * math.pi
-# math.sin(radians)
-#
-# # check
-#
-# math.sqrt(2) / 2.0
+print math # prints information about it
+print math.pi # calling a module's functions (dot notation)
+print math.sqrt(16)
 
-#### Composition
-# An argument of a function can be an expression or even a function calls
-#
-# x = time(3600 + 43)
-# y = type(x)
 
-# Keyword 'def' for Define a function
 print '####'*40
+# Composition
+# An argument of a function can be an expression or even a function call
+print '####'*40
+
+
+print math.sqrt(2+2)
+print math.sqrt(math.sqrt(16))
+
+
+print '####'*40
+# Define a function; Keyword 'def'
+print '####'*40
+
 
 def lyrics():
     print 'Go, tell Aunt Rhodie,'
@@ -49,11 +46,16 @@ def lyrics():
     print 'Go, tell Aunt Rhodie, the'
     print 'goose is in her bed!'
 
-print lyrics()
-print type(lyrics)
+print lyrics # defining a functions creates a variable with the same name
+print type(lyrics) # and it has the type 'function'
 
+print lyrics() # calling a function is the same as calin built-in function
+
+
+print '####'*40
 # Calling a functions inside another function
 print '####'*40
+
 
 def variation():
     print lyrics()
@@ -64,8 +66,11 @@ def variation():
 
 print variation()
 
-# Flow of execution
+
 print '####'*40
+# Flow of execution; follow the flow instead of reading top-bottom!
+print '####'*40
+
 
 def first():
     print 'This is the 1st function'
@@ -84,25 +89,34 @@ def fourth():
 
 print third()
 
+
+print '####'*40
 # NOTE: Parameters vs. Arguments
 # Inside a function arguments are assigned to variables called parameters
+print '####'*40
 
-def repeat(x): # argument
+
+def repeat(x): # argument asiggned to a parameter inside the function
     print x # parameter
     print x # parameter
 
 print repeat('s')
 print repeat(2)
-y = 3
+y = 3 # variable can be used as an argument
 print repeat(y)
 
+
+print '####'*40
 # Local variables and parameters
+print '####'*40
+
 
 def cat_repeat(part1, part2):
     cat = part1 + part2
     print repeat(cat)
 
-print cat_repeat(2, 2)
+print cat_repeat(2, 2) # cat is destroyed when cat_repeat terminates
+
 
 line1 = 'King of the land! King of the land!'
 line2 = 'Beast of the jungle say he is so grand!'

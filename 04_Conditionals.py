be3f2a5b7c788f3698f8e59787d30b4d950807df
Ivo Ivanov <vn.n0t0@gmail.com>
2018-01-02 17:10:41 -0800

    conditionals - updated

--- 04_Conditionals.py
+++ 04_Conditionals.py
@@ -1,7 +1,11 @@
+print '####'*40
 # Modulus operator
+print '####'*40
 
 print 7 / 2
 print 7 % 2 # returns the remainder after division
+print 21 % 3 # if % returns zero one number is divisble by another
+
 
 # NOTE:
 # extracting the right most digit from a number - x % 10 x(in base of 10)
@@ -10,13 +14,16 @@ print 7 % 2 # returns the remainder after division
 print 128 % 10
 print 1024 % 100
 
-
+print '####'*40
 # Booleans - returns True or False values
+print '####'*40
 
 print 4 == 4
 print 4 == 5
 
+print '####'*40
 # Relational operators
+print '####'*40
 
 m = 4
 n = 6
@@ -27,7 +34,9 @@ print m < n  # m is less than n
 print m >= n # m is greater OR equal to n
 print m <= n # m is less or equal to n
 
+print '####'*40
 # Logical operators - and, or, and not
+print '####'*40
 
 a = 4
 print a > 0 and a < 5 # returns True if both are true
@@ -40,7 +49,9 @@ y = 10
 
 print not x > y # returns True if x > y is False
 
+print '####'*40
 # Conditional execution
+print '####'*40
 
 x = 2
 if x > 0:
@@ -49,7 +60,9 @@ if x > 0:
 if x < 0:
     pass    # pass is used when there is no statement in the body
 
+print '####'*40
 # Alternative execution
+print '####'*40
 
 x = 6
 if x%2 == 0:
@@ -57,7 +70,9 @@ if x%2 == 0:
 else:
     print 'x is odd number'
 
+print '####'*40
 # Chained conditionals
+print '####'*40
 
 x = 4
 y = 2
@@ -76,12 +91,16 @@ def draw_b(a):
         print 'The answer is B'
     elif a == 'c':
         print 'The answer is C'
-    else:
-        print 'No answer provided!'
+    # NOTE: if there is a else clause(not required), it has to be at the end
+    # else:
+    #     print 'No answer provided!'
 
 print draw_b('b')
 
+
+print '####'*40
 # Nested conditionals
+print '####'*40
 
 x = 2
 y = 4
@@ -98,9 +117,13 @@ else:
 # Nested conditionals can be difficult to read; logical operators are better
 
 x = 5
+
 if 0 < x:
     if x < 10:
         print 'x is a positive number' # nested
 
 if x > 0 and x < 10:
     print 'x is a positive number' # logical
+
+if x > 10 or x < 6:
+    print 'x is less than 6 or more than 10'

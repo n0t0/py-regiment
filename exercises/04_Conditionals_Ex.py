5d2bb7adfa5a5b890d2aea050f886f3acd4c5d86
Ivo Ivanov <vn.n0t0@gmail.com>
2018-01-03 16:21:34 -0800

    exercise 2 - completed

--- exercises/04_Conditionals_Ex.py
+++ exercises/04_Conditionals_Ex.py
@@ -98,18 +98,3 @@ def is_triangle(a, b, c):
         print 'Yes'
 
 is_triangle(a, b, c)
-
-
-def draw(t, length, n):
-    if n == 0:
-        return
-    angle = 50
-    fd(t, length*n)
-    lt(t, angle)
-    draw(t, length, n-1)
-    rt(t, 2*angle)
-    draw(t, length, n-1)
-    lt(t, angle)
-    bk(t, length*n)
-
-draw(2, 4, 8)

bc5bf24bed5bdd2dd121b20a99b4cd97a24bf60e
Ivo Ivanov <vn.n0t0@gmail.com>
2018-01-03 16:20:46 -0800

    exercise 2 - completed

--- exercises/04_Conditionals_Ex.py
+++ exercises/04_Conditionals_Ex.py
@@ -25,7 +25,7 @@ def check_fermat(a, b, c, n):
 
 check_fermat(a, b, c, n)
 
-#2.
+# 2.
 
 
 def check_fermat(a, b, c, n):
@@ -47,3 +47,69 @@ def check_fermat(a, b, c, n):
             print 'False! Fermat\'s theorem holds'
 
 check_fermat(a, b, c, n)
+
+
+# Exercise 2
+
+# If you are given three sticks, you may or may not be able to arrange them
+# in a triangle. For example, if one of the sticks is 12 inches long and
+# the other two are one inch long, it is clear that you will not be able
+# to get the short sticks to meet in the middle. For any three lengths,
+# there is a simple test to see if it is possible to form a triangle:
+
+# If any of the three lengths is greater than the sum of the other two,
+# then you cannot form a triangle. Otherwise, you can. (If the sum of two
+# lengths equals the third, they form what is called a "degemerate" triangle.)
+
+# 1.
+# Write a function named is_triangle that takes three integers as arguments,
+# and that prints either "Yes" or "No," depending on whether you can or cannot
+# form a triangle from sticks with the given lengths.
+
+# 2.
+# Write a function that prompts the user to input three stick lengths, converts
+# them to integers, and uses is_triangle to check whether sticks with the given
+# lengths can form a triangle.
+
+# 1.
+
+
+def is_triangle(a, b, c):
+    if a > sum((b, c)) or b > sum((a, c)) or c > sum((b, a)):
+        print 'No'
+    else:
+        print 'Yes'
+
+is_triangle(2, 2, 5)
+
+# 2.
+
+
+def is_triangle(a, b, c):
+    print 'Type a lenght for stick A'
+    a = int(raw_input())
+    print 'Type a lenght for stick B'
+    b = int(raw_input())
+    print 'Type a lenght for stick C'
+    c = int(raw_input())
+    if a > sum((b, c)) or b > sum((a, c)) or c > sum((b, a)):
+        print 'No'
+    else:
+        print 'Yes'
+
+is_triangle(a, b, c)
+
+
+def draw(t, length, n):
+    if n == 0:
+        return
+    angle = 50
+    fd(t, length*n)
+    lt(t, angle)
+    draw(t, length, n-1)
+    rt(t, 2*angle)
+    draw(t, length, n-1)
+    lt(t, angle)
+    bk(t, length*n)
+
+draw(2, 4, 8)

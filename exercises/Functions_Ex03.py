2ecd37eced3ac46857e769a8ccfa8443a5aac724
Ivo Ivanov <vn.n0t0@gmail.com>
2018-01-04 17:12:16 -0800

    exercise - added

--- exercises/Functions_Ex03.py
+++ exercises/Functions_Ex03.py
@@ -1,12 +1,12 @@
-# Exercise 3
+# Exercise 1
 
 # Python provides a built-in function called len that returns the length of a string,
 # so the value of len('allen') is 5. Write a function named right_justify that takes
 # a string named s as a parameter and prints the string with enough leading spaces so
 # that the last letter of the string is in column 70 of the display.
 #
-# >>> right_justify('allen')
-#                                                                  allen
+# >>> right_justify('signature')
+#                                                                  signature
 
 s = 'signature'
 
@@ -18,4 +18,41 @@ print right_justify(s)
 
 
 string = 'today is a good day'
-print len(string) * 2 
+print len(string) * 2
+
+# Exercise 2
+# Write a compare function that returns 1 if x > y, 0 if x == y,
+# and -1 if x < y.
+
+x = 2
+y = 1
+
+def compare():
+    if x > y:
+        return 1
+    elif x == y:
+        return 0
+    else:
+        x < y
+        return -1
+
+compare()
+
+def check():
+    return 1
+
+check()
+
+# Exercise 3
+# Use incremental development to write a function called hypotenuse that returns
+# the length of the hypotenuse of a right triangle given the lengths of the two
+# legs as arguments. Record each stage of the development process as you go.
+print '===='*40
+
+from math import sqrt
+
+def hypotenuse(l1, l2):
+    h = l1**2 + l2**2
+    print sqrt(h)
+
+hypotenuse(4, 2)

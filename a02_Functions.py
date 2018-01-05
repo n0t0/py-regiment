988700278ef4b7255706b02396b42b92c5bc8a7f
Ivo Ivanov <vn.n0t0@gmail.com>
2018-01-04 17:24:14 -0800

    advanced functions - added

--- a02_Functions.py
+++ a02_Functions.py
@@ -15,4 +15,37 @@ def absolute_value(x):
     else:
         return x
 
-print 's'
+
+def distance(x1, y1, x2, y2):
+    dx = x2 - x1
+    dy = y2 - y1
+    print 'dx is:', dx
+    print 'dy is:', dy
+    dsquared = dx**2 + dy**2
+    print 'dsquared is:', dsquared
+    result = math.sqrt(dsquared)
+    return result
+
+distance(1,2,4,6)
+
+
+# Composition
+
+
+xc = 2
+yc = 3
+xp = 4
+yp = 6
+
+radius = distance(xc, yc, xp, yp)
+result = area(radius)
+
+
+def circle_area(xc, yc, xp, yp):
+    radius = distance(xc, yc, xp, yp)
+    result = area(radius)
+    return result
+
+
+def circle_area(xc, yc, xp, yp):
+    return area(distance(xc, yc, xp, yp))

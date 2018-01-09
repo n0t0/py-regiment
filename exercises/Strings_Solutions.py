b3c6260135b7bba4ec9b582abb0a28a33b099bc1
Ivo Ivanov <vn.n0t0@gmail.com>
2018-01-08 16:25:41 -0800

    file - rename

--- exercises/Strings_Solutions.py
+++ exercises/Strings_Solutions.py
@@ -44,3 +44,15 @@ for l in prefixes:
         print l + 'uack'
     else:
         print l + suffix
+
+# Exercise 3
+# Modify find so that it has a third parameter, the index in word
+# where it should start looking.
+
+def find(word, letter):
+    index = 0
+    while index < len(word):
+        if word[index] == letter:
+            return index
+        index = index + 1
+    return -1

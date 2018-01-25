f78e497cab91e8c5764c0af560073723ed1c7758
Ivo Ivanov <vn.n0t0@gmail.com>
2018-01-25 09:34:55 -0800

    file - updated

--- 11_Files.py
+++ 11_Files.py
@@ -10,21 +10,21 @@ print fout
 line2 = 'the emblem of our land.\n'
 fout.write(line2)
 
-# fout.close()
+# fout.close() # closeing the file after writing to it
 
-# The argument of write() has to be a string. Anything else needs to be converted
+# NOTE:
+# the argument of write() has to be a string; anything else needs to be converted
 
 # Formatting
 
 x  = 59
-fout.write(str(x))
+# fout.write(str(x))
 
 # option 2 is to use % format operator - modulus
 
 camels = 42
 '%d' % camels
 print 'I have spotted %d camels.' % camels
-
 print 'In %d years I have spotted %g %s.' % (3, 0.1, 'camels')
 
 # Filenames and Paths
@@ -38,17 +38,18 @@ print os.path.abspath('memo.txt') # absolute path
 print os.path.exists('memo.txt') # checking if file exists
 print os.path.isdir('memo.txt') # checking if is a dir
 print os.path.isfile('output.txt') # checking if is a file
-
 print os.listdir(cwd) # returns a list of files in the cwd
 
+
+print '-'
 def walk(dirname):
     for name in os.listdir(dirname):
         path = os.path.join(dirname, name)
-
         if os.path.isfile(path):
             print path
         else:
             walk(path)
+walk(cwd)
 
 # Catching Exceptions
 

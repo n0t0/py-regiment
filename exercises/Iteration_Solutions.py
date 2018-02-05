0dbc8720af5577645bf82ff63d6b307137e6a3fd
Ivo Ivanov <vn.n0t0@gmail.com>
2018-02-05 14:08:54 -0800

    file - updated

--- exercises/Iteration_Solutions.py
+++ exercises/Iteration_Solutions.py
@@ -5,19 +5,26 @@ def print_n(s, n):
     if n <= 0:
         return
     print s
-    print print_n(s, n-1)
+    print_n(s, n-1)
+print_n('flood', 5)
 
-print print_n('flood', 5)
+
+def print_n(s, n):
+    while n != 0:
+        print s
+        n -= 1
+print_n('spam', 5)
+
 
 # Exercise 2
 # Encapsulate this loop
 
-while True:
-    print x
-    y = (x + a/x) / 2
-    if y == x:
-        break
-    x = y
+# while True:
+#     print x
+#     y = (x + a/x) / 2
+#     if y == x:
+#         break
+#     x = y
 
 # in a function called square_root that takes a as a parameter,
 # chooses a reasonable value of x, and returns an estimate of

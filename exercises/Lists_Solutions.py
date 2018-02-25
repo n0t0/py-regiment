6b8d36725add6f3b65e2cde8ecdf31115676071e
Ivo Ivanov <n0t0@Saturn.local>
2018-02-25 12:47:30 -0800

    file - updated

--- exercises/Lists_Solutions.py
+++ exercises/Lists_Solutions.py
@@ -1,26 +1,5 @@
 print '\n Exercise 1\n'
-### Exercise 1
-# Write a function called nested_sum that takes a nested list of integers and
-# add up the elements from all of the nested lists.
-
-# Sometimes you want to traverse one list while building another.
-# For example, the following function takes a list of strings and returns
-# a new list that contains capitalized strings:
-
-# def capitalize_all(t):
-#     res = []
-#     for s in t:
-#         res.append(s.capitalize())
-#     return res
-
-
-# res is initialized with an empty list; each time through the loop, we append
-# the next element. So res is another kind of accumulator.
-
-# An operation like _capitalize_all_ is sometimes called a map because it
-# "maps" a function (in this case the method capitalize) onto each of the
-# elements in a sequence.
-
+# Exercise 1
 
 t = [1, 2, [3, 4], 5, 6, [[7, 8], 9, 10]]
 
@@ -75,29 +54,17 @@ def capitalize_all(t):
         res.append(s.capitalize())
     return res
 
-
 nested_t = ['string1', ['String2', 'string3'], 'string4',['string5']]
-# print type(nested_t)
-print hasattr(str(nested_t), str)
 
 def capitalized_nested(nested_t):
     nested_res = []
-    # index = 0
     for e in nested_t:
         print type(e)
-        # nested_res.append(iter(e))
-            # print 'eda'
-
-            # nested_res.append(e)
-        # print nested_res
-        # print 'dsa'
-        # if e is type(list):
-        # if e == type(list):
-            # print e
-        # print e.capitalize()
-        # if s.isupper()
-        #     nested_tCap.append(s)
-    # return nested_tCap
+        if type(e) == str:
+            nested_res.append(e.capitalize())
+        else:
+            print nested_res.append(capitalize_all(e))
+    print nested_res
 capitalized_nested(nested_t)
 
 

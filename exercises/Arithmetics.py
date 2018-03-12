3ebac63f2276851a278ad414a87c2c8b4bce8e36
Ivo Ivanov <vn.n0t0@gmail.com>
2018-03-12 11:31:04 -0700

    file - updated

---
+++ exercises/Arithmetics.py
@@ -0,0 +1,17 @@
+# If you run a 10 kilometer race in 43 minutes 30 seconds,
+# what is your average time per mile? What is your average
+# speed in miles per hour? (Hint: there are 1.61 kilometers in a mile).
+
+# Speed = Distance / Time
+
+# Convert 10 Kg into miles
+
+print 10 / 1.61
+
+# Convert 43 minutes and 30 seconds to get decimal of a hour
+
+print 43.5 / 60
+
+# Calculate Average Speed
+
+print 6.21 / 0.72

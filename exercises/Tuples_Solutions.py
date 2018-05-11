3f1f8895615c111fc1e3d6af09f609318ca14b6d
Ivo Ivanov <vn.n0t0@gmail.com>
2018-05-11 12:22:28 -0700

    file - update

--- exercises/Tuples_Solutions.py
+++ exercises/Tuples_Solutions.py
@@ -1,17 +1,5 @@
-# Exercise 1
-# Many of the built-in functions use variable-length argument tuples.
-# For example, max and min can take any number of arguments:
-
-# >>> max(1,2,3)
-# 3
-
-# But sum does not.
-
-# >>> sum(1,2,3)
-# TypeError: sum expected at most 2 arguments, got 3
-
-# Write a function called sumall that takes any number of arguments
-# and returns their sum.
+print '\nExercise 1\n'
+# Exercise 1 
 
 a = [1,2,3,4,5,6]
 
@@ -21,56 +9,16 @@ def sumall(*args):
 sumall(1,2,3,5,8,13)
 
 
+print '\nExercise 2\n'
 # Exercise 2
 import random
-words = ['dog', 'monkey', 'fly', 'crocodile', 'horse']
+words = ['dog', 'monkey', 'fly', 'crocodile', 'horse', 'rhino', 'zebra', 'goose', 'camel']
 
-def sort_by_random(words):
+
+def sort_by_length(words):
     t = []
     for word in words:
-        t.append((len(word), random.random(), word))
-
-    t.sort(reverse=True)
-
-    res = []
-    for length, _, word in t:
-        res.append(word)
-    return res
-
-if __name__ == '__main__':
-    words = ['dog', 'monkey', 'fly', 'crocodile', 'horse']
-
-    t = sort_by_random(words)
-    for x in t:
-        print x
-
-
-# NOTE: list first element in a tuple and assign random.random() to those elements
-def sort_by_random(words):
-    t = []
-    for word in words:
-        t.append((len(word), word))
-
-    lt = []
-    for x, word in t:
-        print x
-        lt.append(x)
-    print lt
-
-    dupes = [x for n, x in enumerate(lt) if x in lt[:n]]
-    # dupes = random.random()
-    print dupes
-
-    if dupes in t:
-        # dupes = random.random()
-        print 'ss'
-        # if x == dupes:
-    # [t[i] for i in range(len(t)) if not i == t.index(t[i])]
-    # print t[i]
-    #
-    # for i in range(len(t)):
-    #     if not i == t.index(t[i]):
-    #         print t[i]
+       t.append((len(word), word))
 
     t.sort(reverse=True)
 
@@ -78,20 +26,43 @@ def sort_by_random(words):
     for length, word in t:
         res.append(word)
     return res
+print sort_by_length(words)
+
+
+def sort_by_random(words):
+    t = []
+    for word in words:
+        t.append((len(word), random.random(), word))
+    t.sort(reverse=True)
+
+    res = []
+    for length, _, word in t:
+        res.append(word)
+    return res
 print sort_by_random(words)
 
+if __name__ == '__main__':
+    words = ['dog', 'monkey', 'fly', 'crocodile', 'horse', 'rhino', 'zebra', 'goose', 'camel']
+
+    t = sort_by_random(words)
+    for x in t:
+        print x
+
+
+print '\nExercise 3\n'
 # Exercise 3
-s = 'today is a good day because the dog doesn\'t bark'
-# NOTE: fix: print repeated chars only once
+# s = 'today is a good day because the dog doesn\'t bark'
+s = 'do double gg'
+# TODO: fix: print repeated chars only once -- DONE
+# TODO: replace first for loop with list comprehention -- DONE
+
+
 def most_frequent(s):
-    count = 0
-    lt = []
-    for l in s:
-        # s.sort(reverse=True)
-        print s.count(l), l
-        lt.append((s.count(l), l))
-        # lt.append(l)
-    lt.sort(reverse=True)
-    for elem in lt:
+    lt = [(s.count(l), l) for l in s]
+    uniq = set(lt)
+    for elem in sorted(uniq):
         print elem
 most_frequent(s)
+
+print '\nExercise 4\n'
+# Exercise 4

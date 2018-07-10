eb72468a0115ce3a738388d18cd078f6de5cdd13
Ivo Ivanov <vn.n0t0@gmail.com>
2018-07-10 08:14:17 -0700

    file - update

--- exercises/Inheritance_Solutions.py
+++ exercises/Inheritance_Solutions.py
@@ -8,8 +8,6 @@ class Clock(object):
         self.minute = minute
         self.second = second
         
-    def print_time(self):
-        print '{:02d}:{:02d}:{:02d}'.format(self.hour, self.minute, self.second)
     
     # def time_to_int(self):
     #     minutes = self.hour * 60 + self.minute
@@ -17,26 +15,32 @@ class Clock(object):
     #     return seconds
     #     print seconds
     
+    # def __cmp__(self, other):
+    #     # check hours 
+    #     # if self.hour > other.hour: return 1 
+    #     if self.hour > other.hour: print 'hour' 
+    #     if self.hour < other.hour: return -1
+    #     # if hours are tied - check minutes 
+    #     if self.minute > other.minute: print 'minutes' 
+    #     if self.minute < other.minute: return -1
+    #     # if minutes are tied - check seconds 
+    #     if self.second > other.second: return 1 
+    #     if self.second < other.second: return -1 
+    #     # exact same time
+    #     return 0
+
+    def print_time(self):
+        print '{:02d}:{:02d}:{:02d}'.format(self.hour, self.minute, self.second)
+
     def __cmp__(self, other):
-        # check hours 
-        if self.hour > other.hour return 1 
-        if self.hour < other.hour return -1
-        # if hours are tied - check minutes 
-        if self.minute > other.minute return 1
-        if self.minute < other.minute return -1
-        # if minutes are tied - check seconds 
-        if self.second > other.seconds return 1 
-        if self.second < other.seconds return -1 
-        # exact same time
-        return 0
+        t1 = self.hour, self.minute, self.second
+        t2 = other.hour, other.minute, other.second
+        return cmp(t1, t2)
 
-t1 = Clock(9,2,2)
-t2 = Clock(8,5,2)
-# time.hour = 1
-# time.minute = 40
-# time.second = 52
-
+t1 = Clock(11,4,2)
+t2 = Clock(11,2,2)
 t1.print_time()
 t2.print_time()
-# print time.time_to_int()
+   
+print t1 > t2 
 

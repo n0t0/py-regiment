418ae487ee77ac96e524ff8a98f94b5599d26b1e
Ivo Ivanov <vn.n0t0@gmail.com>
2018-07-18 10:58:20 -0700

    swampy module - added

---
+++ swampy/turtleworld.py
@@ -0,0 +1,19 @@
+from swampy.TurtleWorld import *
+
+world = TurtleWorld()
+bob = Turtle()
+print bob
+
+# fd(bob, 100)
+# lt(bob)
+# fd(bob, 100)
+# lt(bob)
+# fd(bob, 100)
+# lt(bob)
+# fd(bob, 100)
+
+for elt in range(4):
+    fd(bob, 100)
+    lt(bob)
+
+wait_for_user()

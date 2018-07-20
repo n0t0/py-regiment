2f402f72256ffddb730822cb3df5ce184c34c8c1
Ivo Ivanov <vn.n0t0@gmail.com>
2018-07-20 14:59:25 -0700

    file - update

---
+++ wordplay/wordplay.py
@@ -0,0 +1,29 @@
+fin = open('Z:\python\words.txt')
+
+# exercise 1
+for line in fin:
+    word = line.strip()
+    if len(word) > 20:
+        print word
+
+
+# exercise 2 
+word_list = open('Z:\python\words.txt').readlines()
+
+# def has_no_e():
+#     for line in fin:
+#         word = line.strip()
+#         if 'e' not in word:
+#             print word
+# has_no_e()
+
+def has_no_e():
+    has_no_e = []
+    for word in word_list:
+        if 'e' not in word:
+            has_no_e.append(word)
+    p = float(len(has_no_e) / len(word_list))
+    print float(p)
+has_no_e()
+
+    

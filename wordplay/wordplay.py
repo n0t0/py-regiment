233312246977eac23cc1e45b87a4b644911a0a7f
Ivo Ivanov <vn.n0t0@gmail.com>
2018-07-25 09:10:44 -0700

    file - update

--- wordplay/wordplay.py
+++ wordplay/wordplay.py
@@ -3,6 +3,7 @@ from __future__ import division
 fin = open('Z:\python\words.txt')
 
 # exercise 1
+
 for line in fin:
     word = line.strip()
     if len(word) > 20:
@@ -10,15 +11,10 @@ for line in fin:
 
 
 # exercise 2 
-word_list = open('Z:\python\words.txt').readlines()
 
-# print word_list
-# def has_no_e():
-#     for line in fin:
-#         word = line.strip()
-#         if 'e' not in word:
-#             print word
-# has_no_e()
+
+word_list = open('Z:\python\words.txt').readlines()
+word_list = [word.lower().strip() for word in word_list]
 
 
 def has_no_e():
@@ -28,10 +24,12 @@ def has_no_e():
             has_no_e.append(word)
     p = len(has_no_e) / len(word_list)
     print '{:.2f}'.format(p), '%'
+
 has_no_e()
 
     
 # exercise 3
+
 
 def avoids(w, s):
     for w in word_list:
@@ -41,20 +39,18 @@ def avoids(w, s):
 
 # exercise 4
 
-s = 'asta'
-for l in s:
-    print l
 
 def uses_only(word, required_letters):
     # l = []
     for l in required_letters:
         if l in word:
             print 'ok'
+        else:
+            print required_letters, 'not found in', word
+
+uses_only('dp', 'sa')
 
 
-    # for s in raw_input('enter allowed letters: '):
-    #     l.append(s)
-    # print l
-uses_only(fin, 'sa')
+
 
 

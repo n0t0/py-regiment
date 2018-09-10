2532e84b2e04069584361cbe8c00d15d612d66d9
Ivo <vn.n0t0@gmail.com>
2018-09-10 15:12:05 -0700

    file - update

--- libraries/matplotlib/exercises.py
+++ libraries/matplotlib/exercises.py
@@ -1,4 +1,6 @@
 import matplotlib.pyplot as plt 
+import matplotlib.image as mpimg
+import matplotlib as mpl
 import numpy as np 
 
 plt.switch_backend('agg')
@@ -41,16 +43,34 @@ z = x**2
 # ax1 = fig.add_axes([0,0,1,1])
 # ax2 = fig.add_axes([0.2,0.5,.4,.4])
 
+# ax1.plot(x,z)
 # ax1.set_xlabel('x')
 # ax1.set_ylabel('y')
-# ax1.plot(x,z)
 
+# ax2.plot(x,y)
+# ax2.set_title('zoom')
 # ax2.set_ylabel('y')
 # ax2.set_xlabel('x')
 # ax2.set_xlim(xmax=22, xmin=20)
 # ax2.set_ylim(ymax=50, ymin=30)
-# ax2.plot(x,y)
-# ax2.set_title('zoom')
 
 # ax2.set_x
-plt.savefig('ex_03b')
+# plt.savefig('ex_03b')
+
+# Exercise 4 
+
+
+fig,axes = plt.subplots(nrows=1, ncols=2, figsize=(12,2))
+
+axes[0].plot(x,y, lw=3, color='blue')
+axes[0].set_xlim(xmax=100, xmin=0)
+axes[0].set_ylim(ymax=200, ymin=0)
+
+axes[1].plot(x,z, lw=3, color='red', ls='--')
+axes[1].set_xlim(xmax=100, xmin=0)
+axes[1].set_ylim(ymax=10000, ymin=0)
+
+
+plt.tight_layout()
+plt.savefig('ex_04b')
+# plt.show()

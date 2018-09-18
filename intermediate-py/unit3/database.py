053c03f45a432926c93ebaf2cbf92df6082ad4e1
Ivo <vn.n0t0@gmail.com>
2018-09-18 11:42:15 -0700

    files - update

--- intermediate-py/unit3/database.py
+++ intermediate-py/unit3/database.py
@@ -1,6 +1,19 @@
 import sqlite3
 
 connection = sqlite3.connect("jeopardy.db")
-print type(connection)
+# print(type(connection))
+# print(help(connection))
+# print(dir(connection))
 
-curson = connection.curson()
+cursor = connection.cursor()
+# print(dir(cursor))
+
+cursor.execute('SELECT name FROM category LIMIT 10')
+results = cursor.fetchall()
+print(results)
+print(results[1][0])
+
+for category in results: # category mean for tuple in this list
+    print(category[0])
+
+connection.close()

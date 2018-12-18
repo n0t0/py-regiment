2506225f996085ae77931aa824450dc4dff58964
Ivo Ivanov <vn.n0t0@gmail.com>
2018-12-17 19:56:42 -0800

    file - update

--- exercises/Lists_Solutions.py
+++ exercises/Lists_Solutions.py
@@ -327,7 +327,7 @@ if __name__ == '__main__':
     #     print(word, 'in list', in_bisect_cheat(wordlist, word))
 
 
-in_bisect(wordlist, 'd')
+in_bisect(wordlist, 'e')
 
 
 # print '\n Exercise 12\n'.upper()
